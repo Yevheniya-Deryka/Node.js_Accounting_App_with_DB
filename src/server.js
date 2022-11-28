49723098981a3a8ffe12ea03e10e82233607187f
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-11-28 20:27:23 +0200

    Ready to publish

--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@ const port = process.env.PORT || 5000;
 
 const express = require('express');
 
-const { setupTables } = require('./utiles/setupTables.js');
+// const { setupTables } = require('./utiles/setupTables.js');
 const { userRouter } = require('./routers/user.js');
 const { expenseRouter } = require('./routers/expense.js');
 
 const app = express();
 
-setupTables();
+// setupTables();
 
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.use('/hello', (req, res) => {
   res.send('Hello from Expense server');
 });
 
@@ -22,7 +22,4 @@ app.use('/users', userRouter);
 
 app.use('/expenses', expenseRouter);
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server started');
-});
+app.listen(port, '0.0.0.0');
